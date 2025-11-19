7b0d4f5d253ff4c1e7f2f8addb8e4629023872fc
suryaibrahim <ibrahimbtaz@gmail.com>
2025-11-19 12:09:05 +0700

    register mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -28,7 +28,15 @@ class MahasiswaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'nim' => 'required|string|unique:mahasiswas,nim',
+            'jurusan' => 'required|string|max:255',
+            'semester' => 'required|string|max:255',
+            'alamat' => 'required|string|max:500',
+            'no_telp' => 'required|string|max:15',
+            'tanggal_lahir' => 'required|date',
+        ]);
+        
     }
 
     /**
